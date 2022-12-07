b69a1fc3539ecffd6bc932f458be8f769c61d81a
Венцислав Илиев <ultimatemeaning90@gmail.com>
2022-12-07 19:05:13 +0200

    IIFE profound message

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
-import { getCharacters } from './api/dataService.js';
+import { getCharacters } from "./api/dataService.js";
 
 // console.log("script file is loaded")
 
 // *** request test ***
 // check the browser's console
+// Some IFFE functions
 (async () => console.log(await getCharacters()))();
 
 // test
