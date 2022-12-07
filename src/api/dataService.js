4f8f1652a1b1d6190d688c0ef032cd0b752661f1
Венцислав Илиев <ultimatemeaning90@gmail.com>
2022-12-07 19:20:30 +0200

    Normalized data

--- src/api/dataService.js
+++ src/api/dataService.js
@@ -1,9 +1,18 @@
 import { get } from "./api.js";
 
 const endpoints = {
-  "characters": "/character"
-}
+  characters: "/character",
+};
 
 export const getCharacters = async () => {
-  return await get(endpoints.characters)
-}
+  const data = await get(endpoints.characters);
+
+  const normalizedData = data.reduce((acc, cur) => {
+    const id = cur.id;
+
+    acc[id] = cur;
+    return acc;
+  }, {});
+
+  return normalizedData;
+};
